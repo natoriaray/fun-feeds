d98a8a6c194fc2f9c4f53602c70775b1ce777ddf
Natoria Ray <natoriaray@gmail.com>
2019-08-02 13:50:32 -0500

    formatting

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 //create a route
-router.get('/', (req, res) => res.send('Welcome'))
+router.get('/', (req, res) => res.render('welcome'))
 
 module.exports = router;
